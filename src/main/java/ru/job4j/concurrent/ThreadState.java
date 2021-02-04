bd0b9e3e04b0e971e2abb947cff3e84ba0aafec6
ForLearningAtJob4J <youcanwriteme@yandex.ru>
2021-02-04 22:25:11 +0700

    1.1.1.1. Состояние нити. [#461433]

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,15 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(() -> System.out.println(Thread.currentThread().getName()));
+        Thread second = new Thread(() -> System.out.println(Thread.currentThread().getName()));
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED
+                || second.getState() != Thread.State.TERMINATED) {
+            System.out.println("Waiting for termination of all child threads");
+        }
+        System.out.println("All child threads terminated");
+    }
+}
